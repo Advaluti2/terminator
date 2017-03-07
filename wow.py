db2fd448cd55687e81e31774a91088a0dd386507
Michael <advaluti@gmail.com>
2017-03-07 10:29:17 +0000

    Update wow.py

--- wow.py
+++ wow.py
@@ -5,7 +5,7 @@ x = 0
 alpha = ["a", "i", "o", "e", "u", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "w", "v", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "_", " ", "A", "I", "O", "E", "U", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "W", "V", "X", "Y", "Z"]
 beta = [".csv", ".txt", ".png", ".psd", ".lsd", ".pdo", ".json"]
 while True:
-    while x <= 326:
+    while x <= 26:
         filename += random.choice(alpha)
         x +=1
     name = filename + random.choice(beta)
