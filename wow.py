caab2df053f29b5519dec12a405eaef0edd25083
Michael <advaluti@gmail.com>
2017-03-08 13:54:42 +0000

    added more file extensions

--- wow.py
+++ wow.py
@@ -3,7 +3,7 @@ import random
 filename = ""
 x = 0
 alpha = ["a", "i", "o", "e", "u", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "w", "v", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "_", " ", "A", "I", "O", "E", "U", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "W", "V", "X", "Y", "Z"]
-beta = [".csv", ".txt", ".png", ".psd", ".lsd", ".pdo", ".json"]
+beta = [".csv", ".txt", ".png", ".psd", ".lsd", ".pdo", ".json", ".docx", ".pdf"]
 while True:
     while x <= 26:
         filename += random.choice(alpha)
